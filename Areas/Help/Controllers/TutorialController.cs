7465c2c06ff9e93f1391327d43cad2e66a777ccc
dusty91487 <63760861+dusty91487@users.noreply.github.com>
2022-05-05 21:46:13 -0400

    Add project files.

---
+++ Areas/Help/Controllers/TutorialController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebSiteApp2.Areas.Help.Controllers
+{
+    [Area("Help")]
+    public class TutorialController : Controller
+    {
+        [Route("/[area]/[controller]/[action]/Page{id}")]
+        public IActionResult Index(int id)
+        {
+            if (id == 2)
+            {
+                return View("Page2");
+            }
+            else if (id == 3)
+            {
+                return View("Page3");
+            }
+            else 
+            {
+                return View("Page1");
+            }
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+namespace MyWebSiteApp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            var contacts = new List<string>
+            
+                {"555-555-5555",
+                "me@mywebsite.com",
+                "facebook.com/mywebsite"};
+            return View(contacts);
+        }
+    }
+}
